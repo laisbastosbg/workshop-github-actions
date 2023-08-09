473bd1fe7292c958a6ba4379bf4f66df7a939da2
Lais Godinho <lais2bg@gmail.com>
2023-08-09 09:08:03 -0300

    Initial Commit

---
+++ GithubActions/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GithubActions
+//
+//  Created by Lais Godinho on 09/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubActions/GithubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsApp.swift
+//  GithubActions
+//
+//  Created by Lais Godinho on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
